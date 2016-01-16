d3528393f842260dd988630a82924924374fa607
Moyinoluwa <moyinoluwaa@gmail.com>
2016-01-16 03:04:29 +0100

    Initial commit

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -0,0 +1,212 @@
+package com.example.xyzreader.ui;
+
+import android.app.Fragment;
+import android.app.LoaderManager;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.format.DateUtils;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.example.xyzreader.R;
+import com.example.xyzreader.data.ArticleLoader;
+
+/**
+ * A fragment representing a single Article detail screen. This fragment is
+ * either contained in a {@link ArticleListActivity} in two-pane mode (on
+ * tablets) or a {@link ArticleDetailActivity} on handsets.
+ */
+public class ArticleDetailFragment extends Fragment implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+    private static final String TAG = "ArticleDetailFragment";
+
+    public static final String ARG_ITEM_ID = "item_id";
+    private static final float PARALLAX_FACTOR = 1.25f;
+
+    private Cursor mCursor;
+    private long mItemId;
+    private View mRootView;
+    private int mMutedColor = 0xFF333333;
+
+    private boolean mIsCard = false;
+    private int mStatusBarFullOpacityBottom;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public ArticleDetailFragment() {
+    }
+
+    public static ArticleDetailFragment newInstance(long itemId) {
+        Bundle arguments = new Bundle();
+        arguments.putLong(ARG_ITEM_ID, itemId);
+        ArticleDetailFragment fragment = new ArticleDetailFragment();
+        fragment.setArguments(arguments);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            mItemId = getArguments().getLong(ARG_ITEM_ID);
+        }
+
+        mIsCard = getResources().getBoolean(R.bool.detail_is_card);
+        mStatusBarFullOpacityBottom = getResources().getDimensionPixelSize(
+                R.dimen.detail_card_top_margin);
+        setHasOptionsMenu(true);
+    }
+
+    public ArticleDetailActivity getActivityCast() {
+        return (ArticleDetailActivity) getActivity();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // In support library r8, calling initLoader for a fragment in a FragmentPagerAdapter in
+        // the fragment's onCreate may cause the same LoaderManager to be dealt to multiple
+        // fragments because their mIndex is -1 (haven't been added to the activity yet). Thus,
+        // we do this in onActivityCreated.
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mRootView = inflater.inflate(R.layout.fragment_article_detail, container, false);
+        mRootView.findViewById(R.id.share_fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(getActivity())
+                        .setType("text/plain")
+                        .setText("Some sample text")
+                        .getIntent(), getString(R.string.action_share)));
+            }
+        });
+
+        Toolbar toolbar = (Toolbar) mRootView.findViewById(R.id.toolbar);
+
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        bindViews();
+        return mRootView;
+    }
+
+    static float progress(float v, float min, float max) {
+        return constrain((v - min) / (max - min), 0, 1);
+    }
+
+    static float constrain(float val, float min, float max) {
+        if (val < min) {
+            return min;
+        } else if (val > max) {
+            return max;
+        } else {
+            return val;
+        }
+    }
+
+    private void bindViews() {
+        if (mRootView == null) {
+            return;
+        }
+
+        TextView titleView = (TextView) mRootView.findViewById(R.id.article_title);
+        TextView timeview = (TextView) mRootView.findViewById(R.id.article_time);
+        TextView authorView = (TextView) mRootView.findViewById(R.id.article_author);
+        TextView bodyView = (TextView) mRootView.findViewById(R.id.article_body);
+        final ImageView mPhotoView = (ImageView) mRootView.findViewById(R.id.photo);
+
+        if (mCursor != null) {
+            mRootView.setAlpha(0);
+            mRootView.setVisibility(View.VISIBLE);
+            mRootView.animate().alpha(1);
+            timeview.setText(Html.fromHtml(
+                    DateUtils.getRelativeTimeSpanString(
+                            mCursor.getLong(ArticleLoader.Query.PUBLISHED_DATE),
+                            System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
+                            DateUtils.FORMAT_ABBREV_ALL).toString()));
+            titleView.setText(mCursor.getString(ArticleLoader.Query.TITLE));
+            authorView.setText("by " + mCursor.getString(ArticleLoader.Query.AUTHOR));
+            bodyView.setText(Html.fromHtml(mCursor.getString(ArticleLoader.Query.BODY)));
+            ImageLoaderHelper.getInstance(getActivity()).getImageLoader()
+                    .get(mCursor.getString(ArticleLoader.Query.PHOTO_URL), new ImageLoader.ImageListener() {
+                        @Override
+                        public void onResponse(ImageLoader.ImageContainer imageContainer, boolean b) {
+                            Bitmap bitmap = imageContainer.getBitmap();
+                            if (bitmap != null) {
+                                Palette p = Palette.generate(bitmap, 12);
+                                mMutedColor = p.getDarkMutedColor(0xFF333333);
+                                mPhotoView.setImageBitmap(imageContainer.getBitmap());
+                                // mRootView.findViewById(R.id.meta_bar)
+                                //   .setBackgroundColor(mMutedColor);
+                            }
+                        }
+
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
+
+                        }
+                    });
+        } else {
+            mRootView.setVisibility(View.GONE);
+            timeview.setText("N/A");
+            titleView.setText("N/A");
+            authorView.setText("N/A");
+            bodyView.setText("N/A");
+        }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return ArticleLoader.newInstanceForItemId(getActivity(), mItemId);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        if (!isAdded()) {
+            if (cursor != null) {
+                cursor.close();
+            }
+            return;
+        }
+
+        mCursor = cursor;
+        if (mCursor != null && !mCursor.moveToFirst()) {
+            Log.e(TAG, "Error reading item detail cursor");
+            mCursor.close();
+            mCursor = null;
+        }
+
+        bindViews();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+        mCursor = null;
+        bindViews();
+    }
+}
